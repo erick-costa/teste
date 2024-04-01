775fe1bb3b3208efb937f1c719e2e88cbb653fd3
erick-costa <erickcosta2509@gmail.com>
2024-04-01 10:27:30 -0300

    inicializando projeto

---
+++ src/test/java/com/generation/zerone/ZeroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.zerone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
